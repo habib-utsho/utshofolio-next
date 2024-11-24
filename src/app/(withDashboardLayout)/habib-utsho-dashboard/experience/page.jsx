5be55d1a6cd5f0eeb9742b7e78cdcb8d14176900
Source-Code-007 <utsho926@gmail.com>
2024-11-25 01:25:10 +0600

    exp updated

--- src/app/(withDashboardLayout)/habib-utsho-dashboard/experience/page.jsx
+++ src/app/(withDashboardLayout)/habib-utsho-dashboard/experience/page.jsx
@@ -1,6 +1,16 @@
 "use client";
 
-import { Empty, Table, Popconfirm, Skeleton, Button } from "antd";
+import {
+  Empty,
+  Table,
+  Popconfirm,
+  Skeleton,
+  Button,
+  Modal,
+  Form,
+  message,
+  Switch,
+} from "antd";
 import { DeleteFilled, EditOutlined } from "@ant-design/icons";
 import {
   useDeleteExperience,
@@ -8,9 +18,25 @@ import {
 } from "@/hooks/experience.hook";
 import { useState, useEffect } from "react";
 
+import MyInp from "@/ui/Form/MyInp";
+import "react-quill/dist/quill.snow.css";
+import {
+  useCreateExperience,
+  useUpdateExperience,
+} from "@/hooks/experience.hook";
 import dynamic from "next/dynamic";
 
-const ExperienceModal = dynamic("./_modal/ExperienceModal", { ssr: false });
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+
+const toolbarOptions = [
+  [{ header: [1, 2, false] }],
+  ["bold", "italic", "underline"],
+  ["blockquote", "code-block"],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ color: [] }, { background: [] }], // Dropdown for color
+  ["link", "image"],
+  ["clean"], // Remove formatting button
+];
 
 const Experience = () => {
   const [pagination, setPagination] = useState({ limit: 10, page: 1 });
@@ -18,6 +44,13 @@ const Experience = () => {
   const [editingExperience, setEditingExperience] = useState(null);
   const [isExperienceModalVisible, setIsExperienceModalVisible] =
     useState(false);
+
+  const [form] = Form.useForm();
+  const { mutate: createExperience, isPending: isLoadingCreateExperience } =
+    useCreateExperience();
+  const { mutate: updateExperience, isPending: isLoadingUpdateExperience } =
+    useUpdateExperience();
+  const [description, setDescription] = useState("");
 
   const { data: experiences, isPending: isLoadingExperiences } =
     useGetAllExperiences([
@@ -98,6 +131,44 @@ const Experience = () => {
     }
   }, [isLoadingDeleteExperience, setIsLoadingDeleteId]);
 
+  useEffect(() => {
+    if (editingExperience) {
+      form.setFieldsValue({
+        ...editingExperience,
+      });
+
+      setDescription(editingExperience?.description);
+    }
+  }, [editingExperience, form]);
+
+  const handleCreateUpdateExperience = async (values) => {
+    try {
+      values.description = description;
+
+      const action = editingExperience ? updateExperience : createExperience;
+      const onSuccessMessage = editingExperience
+        ? "Experience updated successfully"
+        : "Experience created successfully";
+      const payload = editingExperience
+        ? { payload: values, id: editingExperience._id }
+        : values;
+
+      action(payload, {
+        onSuccess: (res) => {
+          message.success(res?.message || onSuccessMessage);
+          form.resetFields();
+          setIsExperienceModalVisible(false);
+          if (editingExperience) {
+            setEditingExperience(null);
+          }
+          setDescription("");
+        },
+      });
+    } catch (error) {
+      message.error(error?.message || "Failed to process experience");
+    }
+  };
+
   return (
     <div className="p-6">
       <div className="flex justify-between items-center gap-4 flex-wrap">
@@ -138,12 +209,101 @@ const Experience = () => {
       )}
 
       {/* Modal */}
-      <ExperienceModal
-        visible={isExperienceModalVisible}
-        setVisible={setIsExperienceModalVisible}
-        editingExperience={editingExperience}
-        setEditingExperience={setEditingExperience}
-      />
+      <Modal
+        title={
+          editingExperience ? "Update Experience" : "Create New Experience"
+        }
+        width={800}
+        open={isExperienceModalVisible}
+        onCancel={() => {
+          setIsExperienceModalVisible(false);
+          setEditingExperience(null);
+          form.resetFields();
+        }}
+        footer={null}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={handleCreateUpdateExperience} // Fixed the event handler to use onFinish
+        >
+          <MyInp
+            type="text"
+            name="companyName"
+            label="Company Name"
+            rules={[
+              { required: true, message: "Please input the company name!" },
+            ]}
+            placeholder="Enter company name"
+          />
+          <MyInp
+            type="text"
+            name="role"
+            label="Role"
+            rules={[{ required: true, message: "Please input the role!" }]}
+            placeholder="Enter role"
+          />
+          <MyInp
+            type="text"
+            name="location"
+            label="Location"
+            placeholder="Enter location"
+          />
+
+          {/* Quill editor */}
+          <div className="my-2">
+            <ReactQuill
+              theme="snow"
+              value={description}
+              onChange={setDescription}
+              placeholder="Enter your experience description"
+              modules={{
+                toolbar: toolbarOptions,
+              }}
+            />
+          </div>
+
+          <MyInp
+            type="text"
+            name="timePeriod"
+            label="Time Period"
+            rules={[
+              { required: true, message: "Please input the Time Period!" },
+            ]}
+            placeholder="Enter time period"
+          />
+
+          <MyInp
+            type="select"
+            name="jobType"
+            label="Job Type"
+            rules={[{ required: true, message: "Please select a job type!" }]}
+            options={[
+              { value: "Remote", label: "Remote" },
+              { value: "On Site", label: "On Site" },
+              { value: "Hybrid", label: "Hybrid" },
+            ]}
+            placeholder="Select a job type"
+          />
+
+          <Form.Item label="Is Course?" name={"isCourse"}>
+            <Switch
+              checkedChildren="Yes"
+              unCheckedChildren="No"
+              defaultChecked={false}
+              onChange={(checked) => form.setFieldsValue({ isCourse: checked })}
+            />
+          </Form.Item>
+
+          <Button
+            loading={isLoadingCreateExperience || isLoadingUpdateExperience}
+            type="primary"
+            htmlType="submit"
+          >
+            {editingExperience ? "Update Experience" : "Create Experience"}
+          </Button>
+        </Form>
+      </Modal>
     </div>
   );
 };
